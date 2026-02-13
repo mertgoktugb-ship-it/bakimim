d5584fbefc022ca21263c20b8fdae69ded0cb08f
mertgoktugb-ship-it <mertgoktugb@gmail.com>
2026-02-14 01:52:15 +0300

    add initial landing page content

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', marginTop: '100px' }}>
+      <h1>Bakımım.com Yakında Burada!</h1>
+      <p>Şen Kardeşler sunar...</p>
+    </div>
+  )
+}
